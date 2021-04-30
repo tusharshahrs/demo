e450969526d78bdba6e212b38e23c9b20861e66c
Tushar Shah <tushar.shah1@gmail.com>
2021-04-30 11:17:18 -0400

    azure serverless workshop labl 2

---
+++ content/lab/pulumi/azure-native/python/app/hello/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+import azure.functions as func
+import logging
+
+
+def main(req: func.HttpRequest,
+         obj: func.InputStream):
+
+    logging.info(f'Python HTTP triggered function processed: {obj.read()}')
