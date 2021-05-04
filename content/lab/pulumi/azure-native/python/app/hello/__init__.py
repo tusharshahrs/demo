aeae7ffc6c5ee18c50ccd1acc649cd10b7489a16
Tushar Shah <tushar.shah1@gmail.com>
2021-05-04 08:12:11 -0400

    updated with logging and return

--- content/lab/pulumi/azure-native/python/app/hello/__init__.py
+++ content/lab/pulumi/azure-native/python/app/hello/__init__.py
@@ -1,9 +1,3 @@
-# __init__.py
-import azure.functions as func
-import logging
-
-
-def main(req: func.HttpRequest,
-         obj: func.InputStream):
-
-    logging.info(f'Python HTTP triggered function processed: {obj.read()}')
+def main(req):
+     logging.info('Python HTTP trigger function processed a request.')
+     return "You've successfully deployed a Function App!"
